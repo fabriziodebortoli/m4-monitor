551a736e12532ffa1df9585dbdab63cba30f2054
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2019-02-07 11:15:19 +0100

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,39 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    instanceKey: 'I-41E07E',
+    logger: {
+        url: 'https://gwam.mago.cloud/logger/api/'
+    },
+    stompConfig: {
+        // Which server?
+        url: 'ws://127.0.0.1:15674/ws',
+        // Headers
+        // Typical keys: login, passcode, host
+        headers: {
+            login: 'guest',
+            passcode: 'guest'
+        },
+        // How often to heartbeat?
+        // Interval in milliseconds, set to 0 to disable
+        heartbeat_in: 0, // Typical value 0 - disabled
+        heartbeat_out: 20000, // Typical value 20000 - every 20 seconds
+        // Wait in milliseconds before attempting auto reconnect
+        // Set to 0 to disable
+        // Typical value 5000 (5 seconds)
+        reconnect_delay: 5000,
+        // Will log diagnostics on console
+        debug: true
+    }
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import 'hammerjs';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { environmentLoader as environmentLoaderPromise } from './environments/environmentLoader';
 
-if (environment.production) {
-  enableProdMode();
-}
+environmentLoaderPromise.then(env => {
+    if (env.production) {
+        enableProdMode();
+    }
+    environment.logger = env.logger;
+    environment.instanceKey = env.instanceKey;
+    environment.stompConfig = env.stompConfig;
+    if (!environment.stompConfig) console.error('Stomp disabled');
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    platformBrowserDynamic()
+        .bootstrapModule(AppModule)
+        .catch(err => console.log(err));
+});
