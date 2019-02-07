c0bd0d818a110a4ba5c68258a2c7f89bf056d5c6
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2019-02-07 11:43:19 +0100

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'm4-monitor';
+  title = 'ng-pwa-basic';
 }
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/m4-monitor'),
+      dir: require('path').join(__dirname, '../coverage/ng-pwa-basic'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
